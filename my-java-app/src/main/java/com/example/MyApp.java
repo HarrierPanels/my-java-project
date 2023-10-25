ef09143fad70577e6198c6109c5b5047741d56d6
HarrierPanels <adsnse@gmail.com>
2023-10-25 19:37:04 +0000

    CI Pipeline

--- my-java-app/src/main/java/com/example/MyApp.java
+++ my-java-app/src/main/java/com/example/MyApp.java
@@ -1,7 +1,30 @@
 package com.example;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
 public class MyApp {
-    public static void main(String[] args) {
-        System.out.println("Hello, Maven!");
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create();
+        server.bind(new InetSocketAddress(80), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server started on port 80");
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "Hello, Maven!";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
     }
 }
